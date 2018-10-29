7f8af08e3bfe0459e6d9087504e7bfcca17537e4
richard.kunze <richard.kunze@daqana.com>
2018-10-29 11:08:58 +0100

    Re-export text/range_filter

--- R/filtering.R
+++ R/filtering.R
@@ -6,7 +6,19 @@ get_filters <- function(input, context) {
   vals
 }
 
+#' @title text filter for data.frames
+#'
+#' @description text_filter filters a given data frame with the given filter
+#' values. Names of the given values vector should be the indices of the
+#' corresponding data frame columns. All filters are case-ignoring.
+#'
+#' @param df data frame to filter
+#' @param values character array with the filter values, should have length of
+#' data or being named
+#'
+#' @return text_filter: filtered data frame
 #' @author richard.kunze
+#' @export
 text_filter <- function(df, values) {
   if (all(values == "")) return(df)
   values <- unlist(values)
@@ -20,7 +32,7 @@ text_filter <- function(df, values) {
 }
 
 #' @author richard.kunze
-text_filter_rec <- function(df, values, valid, depth = 1L) {
+text_filter_rec <- function(df, values, valid, depth = 1) {
   if (depth > length(values)) return(df[valid, , drop = FALSE])
   if (!is.na(values[depth])) {
     index <- tryCatch(
@@ -29,15 +41,26 @@ text_filter_rec <- function(df, values, valid, depth = 1L) {
     ) # catch invalid regex
     valid <- valid[index]
   }
-  return(text_filter_rec(df, values, valid, depth + 1L))
+  return(text_filter_rec(df, values, valid, depth + 1))
 }
 
+#' @title numeric range filter for data.frames
+#'
+#' @description range_filter filters a given data frame with the given filter
+#' values. Names of the given ranges vector should be the indices of the
+#' corresponding data frame columns.
+#'
+#' @param ranges numeric (or numeric makeable) vector with the filter ranges,
+#' should have length of data or being named
+#'
+#' @return range_filter: filtered data frame
 #' @author richard.kunze
+#' @export
 range_filter <- function(df, ranges) {
   if (is.atomic(ranges) && length(ranges) == 2) ranges <- list(ranges)
   newRanges <- lapply(ranges, function(x) {
     vals <- suppressWarnings(as.numeric(x))
-    if (length(x) == 2L && all(!is.na(vals))) vals
+    if (length(x) == 2 && all(!is.na(vals))) vals
     else NA
   })
   if (!is.null(names(ranges))) {
@@ -50,11 +73,11 @@ range_filter <- function(df, ranges) {
 }
 
 #' @author richard.kunze
-range_filter_rec <- function(df, ranges, valid, depth = 1L) {
+range_filter_rec <- function(df, ranges, valid, depth = 1) {
   if (depth > length(ranges) || length(valid) == 0) return(df[valid,])
   if (all(!is.na(ranges[depth]))) {
     tmp <- suppressWarnings(as.numeric(df[valid, depth]))
-    valid <- valid[!is.na(tmp) & tmp >= ranges[[depth]][1L] & tmp <= ranges[[depth]][2L]]
+    valid <- valid[!is.na(tmp) & tmp >= ranges[[depth]][1] & tmp <= ranges[[depth]][2]]
   }
-  return(range_filter_rec(df, ranges, valid, depth + 1L))
+  return(range_filter_rec(df, ranges, valid, depth + 1))
 }
