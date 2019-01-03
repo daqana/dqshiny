9d3529d69236269d48c6ab2c3cd1fdd7cabb5ec4
richard.kunze <richard.kunze@daqana.com>
2019-01-03 16:01:48 +0100

    Add header and borders support to html tables

--- R/dq_htmltable.R
+++ R/dq_htmltable.R
@@ -8,11 +8,19 @@
 #' the alignment of every cell.
 #'
 #' @param elements List of elements to show in the list, each element of the
-#'   list should be a row of the table.
+#'   list should be a row of the table. Can also be a data.frame with names used
+#'   as header.
 #' @param id Optional, character specifying the elements id.
 #' @param align Optional, character (vector) or list of characters showing the
-#'   alignment of the table or each column/cell, can be one of "left", "right"
+#'   alignment of the table for each column/cell, can be one of "left", "right"
 #'   or "center", defaults to "left", can be abbreviated.
+#' @param head_align Optional character vector of header alignments, defaults to
+#' "center", can be abbreviated.
+#' @param borders Optional character specifying the desired borders to show. Can
+#'   be either a single character vector of length one with one of c("inner",
+#'   "outer", "all", "tex") or a list of character vectors specifying all
+#'   borders by hand. Possible values are "top", "right", "bottom", "left". The
+#'   first entry of the list will be used for the header row if given.
 #' @author richard.kunze
 #' @return HTML table
 #' @export
@@ -21,50 +29,76 @@
 #' if (interactive()) {
 #'
 #' library(shiny)
+#' lets <- data.frame(lower = letters[1:5], UPPER = LETTERS[1:5])
 #' shinyApp(
 #'   ui = fluidPage(
-#'     fluidRow(
-#'       column(3, dq_htmltable(list(list("Description", icon("hashtag")),
-#'                              list("Value", textInput("value", NULL)),
-#'                              list("Result", textOutput("result")))),
-#'                              offset = 2, style="background:#ff8f00"
-#'       ),
-#'       column(3, dq_htmltable(list(list("Left", "Center", "Right"),
-#'                              list("Center", "Right", "Left")),
-#'                              align = list(c("left", "center", "right"),
-#'                              c("center", "right", "left"))),
-#'                              offset = 1, style="background:#ff8f00"
-#'       )
+#'     dq_space(), fluidRow(
+#'       column(3, dq_htmltable(
+#'         list(list("Description", icon("hashtag")),
+#'              list("Value", textInput("value", NULL)),
+#'              list("Result", textOutput("result")))
+#'       )),
+#'       column(2, dq_htmltable(lets, borders = "outer")),
+#'       column(3, dq_htmltable(
+#'         list(c("Left", "Center", "Right"), c("Center", "Right", "Left")),
+#'         align = list(c("LEFT", "center", "right"), c("c", "R", "l")),
+#'         borders = "all"
+#'       )),
+#'       column(2, dq_htmltable(lets, borders = "tex")),
+#'       column(2, dq_htmltable(lets, borders = list(
+#'         c("top left", "bottom right"), # header
+#'         c("", ""), c("", ""), c("", ""), c("", ""), c("bottom", "bottom")
+#'       )))
 #'     )),
 #'   server = function(input, output) {
-#'     observeEvent(input$value, output$result <- renderText(input$value))
+#'     output$result <- renderText(input$value)
 #'   }
 #' )
 #'
 #' }
-dq_htmltable <- function(elements, id = NULL, align = "left") {
+dq_htmltable <- function(
+  elements, id = NULL,
+  align = "left", head_align = "center",
+  borders = "inner"
+) {
 
-  if (is.null(align)) {
-    align <- "left"
+  els <- elements
+  if (is.data.frame(els)) els <- as.data.frame(t(els))
+
+  header <- names(elements)
+  align <- fill_alignments(align, els)
+  head_align <- fill_alignments(head_align, header)
+
+  if (is.list(borders)) {
+    if (is.null(header)) {
+      head_borders <- NULL
+      body_borders <- borders
+    } else {
+      head_borders <- as.list(borders[[1]])
+      body_borders <- lapply(borders[-1], as.list)
+    }
+  } else {
+    head_borders <- head_borders(borders, header)
+    body_borders <- fill_borders(borders, els)
   }
 
-  if (length(align) != length(elements[[1]]) || is.list(align)) {
-    align <- lapply(align, function(a) { rep_len(a, length(elements[[1]])) })
-  }
-
-  if (!is.list(align) || length(align) != length(elements)) {
-    align <- lapply(seq(elements), function(x) { unlist(align) })
-  }
-
-  align <- fill_alignments(align)
   shiny::tags$table(
-    style = "width:100%;", id = id,
-    lapply(seq(elements), function(r) {
+    class = "dq-html-table", id = id,
+    if (!is.null(header)) {
       shiny::tags$tr(
-        lapply(seq(elements[[r]]), function(c) {
+        lapply(seq(header), function(i) {
+          shiny::tags$th(
+            header[[i]], class = paste(head_align[[i]], head_borders[[i]])
+          )
+        })
+      )
+    },
+    lapply(seq(els), function(r) {
+      shiny::tags$tr(
+        lapply(seq(els[[r]]), function(c) {
           shiny::tags$td(
-            elements[[r]][c],
-            style = paste0("text-align:", align[[r]][c], ";")
+            els[[r]][c],
+            class = paste(align[[r]][[c]], body_borders[[r]][[c]])
           )
         })
       )
@@ -74,12 +108,108 @@ dq_htmltable <- function(elements, id = NULL, align = "left") {
 
 }
 
-fill_alignments <- function(align) {
+fill_alignments <- function(align, els) {
+
+  if (is.null(align)) {
+    align <- "left"
+  }
+
+  if (length(align) != length(els[[1]]) || is.list(align)) {
+    align <- lapply(align, rep_len, length(els[[1]]))
+  }
+
+  if (!is.list(align) || length(align) != length(els)) {
+    align <- lapply(seq(els), function(x) { unlist(align) })
+  }
 
   lapply(align, function(a) {
-    a <- gsub("^r$", "right", a)
+    a <- gsub("^r$", "right", tolower(a))
     a <- gsub("^l$", "left", a)
-    gsub("^c$", "center", a)
+    paste0("align-", gsub("^c$", "center", a))
   })
+
+}
+
+fill_borders <- function(type, els) {
+
+  r_max <- length(els)
+  c_max <- length(els[[1L]])
+  rows <- seq(r_max)
+  cols <- seq(c_max)
+
+  if (isTRUE(type == "inner")) {
+    lapply(rows, function(r) {
+      lapply(cols, function(c) {
+        paste(c(
+          if (c > 1) "left", if (c < c_max) "right",
+          if (r > 1) "top", if (r < r_max) "bottom"
+        ), collapse = " ")
+      })
+    })
+  } else if (isTRUE(type == "outer")) {
+    lapply(rows, function(r) {
+      lapply(cols, function(c) {
+        paste(c(
+          if (c == 1) "left", if (c == c_max) "right",
+          if (r == 1) "top", if (r == r_max) "bottom"
+        ), collapse = " ")
+      })
+    })
+  } else if (isTRUE(type == "all")) {
+    lapply(rows, function(r) {
+      lapply(cols, function(c) {
+        paste(c(
+          "right", if (c == 1) "left",
+          "bottom", if (r == 1) "top"
+        ), collapse = " ")
+      })
+    })
+  } else if (isTRUE(type == "tex")) {
+    lapply(rows, function(r) {
+      lapply(cols, function(c) {
+        paste(c(
+          if (r == r_max) "bottom", if (r == 1) "top"
+        ), collapse = " ")
+      })
+    })
+  } else {
+    lapply(rows, function(r) {
+      lapply(cols, function(c) { "" })
+    })
+  }
+
+}
+
+head_borders <- function(type, header) {
+
+  c_max <- length(header)
+  cols <- seq(c_max)
+  if (isTRUE(type == "inner")) {
+    lapply(seq(header), function(c) {
+      paste(c(
+        if (c > 1) "left", if (c < c_max) "right", "bottom"
+      ), collapse = " ")
+    })
+  } else if (isTRUE(type == "outer")) {
+    lapply(seq(header), function(c) {
+      paste(c(
+        if (c == 1) "left", if (c == c_max) "right", "top", "bottom"
+      ), collapse = " ")
+    })
+  } else if (isTRUE(type == "all")) {
+    lapply(seq(header), function(c) {
+      paste(c(
+        if (c == 1) "left", "right", "top", "bottom"
+      ), collapse = " ")
+    })
+  } else if (isTRUE(type == "tex")) {
+    lapply(seq(header), function(c) {
+      paste(c(
+        "top", "bottom"
+      ), collapse = " ")
+    })
+  } else {
+    lapply(seq(header), function(c) { "" })
+  }
 
 }
--- tests/testthat/test-dq_htmltable.R
+++ tests/testthat/test-dq_htmltable.R
@@ -1,7 +1,7 @@
 context("dq_htmltable / dq_htmltable")
 
 test_that("bad inputs work", {
-  expect_equal(dq_htmltable(NULL, NULL, NULL), shiny::tags$table(list(), style = "width:100%;", init()))
+  expect_equal(dq_htmltable(NULL, NULL, NULL), shiny::tags$table(class="dq-html-table", NULL, list(), init()))
 })
 
 test_that("alignments work", {
@@ -9,9 +9,40 @@ test_that("alignments work", {
   a <- list(c("left", "left"), c("left", "left"))
   expect_equal(dq_htmltable(el), dq_htmltable(el, align = a))
   expect_equal(dq_htmltable(el, align = "l"), dq_htmltable(el, align = a))
-  expect_equal(dq_htmltable(el, align = "left"), dq_htmltable(el, align = a))
   expect_equal(dq_htmltable(el, align = c("left", "l")), dq_htmltable(el, align = a))
-  expect_equal(dq_htmltable(el, align = list("l", "left")), dq_htmltable(el, align = a))
-  expect_equal(dq_htmltable(el, align = list(c("l", "left"), "left")), dq_htmltable(el, align = a))
-  expect_equal(dq_htmltable(el, align = list(c("l", "l"), c("l", "l"))), dq_htmltable(el, align = a))
+  expect_equal(dq_htmltable(el, align = list("L", "lEft")), dq_htmltable(el, align = a))
+})
+
+test_that("borders without header work", {
+  el <- list(c("a", "b"), c("c", "d"))
+  expect_silent(res <- dq_htmltable(el))
+  expect_true(grepl("right bottom$", res$children[[2]][[1]]$children[[1]][[1]]$attribs$class))
+  expect_silent(res <- dq_htmltable(el, borders = "outer"))
+  expect_true(grepl("left top$", res$children[[2]][[1]]$children[[1]][[1]]$attribs$class))
+  expect_silent(res <- dq_htmltable(el, borders = "tex"))
+  expect_true(grepl("top$", res$children[[2]][[1]]$children[[1]][[1]]$attribs$class))
+  expect_silent(res <- dq_htmltable(el, borders = "all"))
+  expect_true(grepl("right left bottom top$", res$children[[2]][[1]]$children[[1]][[1]]$attribs$class))
+  expect_silent(res <- dq_htmltable(el, borders = list(
+    c("left right", ""), c("", "")
+  )))
+  expect_true(grepl("left right$", res$children[[2]][[1]]$children[[1]][[1]]$attribs$class))
+  expect_silent(res <- dq_htmltable(el, borders = NULL))
+})
+
+test_that("borders with header work", {
+  el <- data.frame(A = c("a", "b"), B = c("c", "d"))
+  expect_silent(res <- dq_htmltable(el))
+  expect_true(grepl("right bottom$", res$children[[1]]$children[[1]][[1]]$attribs$class))
+  expect_silent(res <- dq_htmltable(el, borders = "outer"))
+  expect_true(grepl("left top bottom$", res$children[[1]]$children[[1]][[1]]$attribs$class))
+  expect_silent(res <- dq_htmltable(el, borders = "tex"))
+  expect_true(grepl("top bottom$", res$children[[1]]$children[[1]][[1]]$attribs$class))
+  expect_silent(res <- dq_htmltable(el, borders = "all"))
+  expect_true(grepl("left right top bottom$", res$children[[1]]$children[[1]][[1]]$attribs$class))
+  expect_silent(res <- dq_htmltable(el, borders = list(
+    c("left right", ""), c("", ""), c("", "")
+  )))
+  expect_true(grepl("left right$", res$children[[1]]$children[[1]][[1]]$attribs$class))
+  expect_silent(res <- dq_htmltable(el, borders = NULL))
 })
