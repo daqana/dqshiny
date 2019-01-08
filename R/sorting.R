68ddbec21e34d1129c4ec5984d6a4951663716ac
richard.kunze <richard.kunze@daqana.com>
2019-01-08 13:01:52 +0100

    Move sort button updates to dir/col change events

--- R/sorting.R
+++ R/sorting.R
@@ -1,5 +1,5 @@
 #' @author richard.kunze
-add_sorting_observer <- function(input, session, dq_values, context, paged, page_id) {
+add_sorting_observer <- function(input, session, dq_values, context, page_size, page_id) {
   dirs <- c("", "up", "down")
   sorts <- paste("sort", context, shiny::isolate(names(dq_values[[context]])), sep = "_")
   ignore <- structure(rep(TRUE, length(sorts)), names = sorts)
@@ -9,26 +9,30 @@ add_sorting_observer <- function(input, session, dq_values, context, paged, page
       if (ignore[s]) {
         ignore[s] <<- FALSE
       } else {
-        dq_values$sort_col <- i
-        dq_values$sort_dir <- dirs[input[[s]]]
-        lapply(sorts[sorts != s], function(n) {
-          if (length(input[[n]]) == 1L && input[[n]] != 1L) {
-            ignore[n] <<- TRUE
-            update_icon_state_button(session, n, value = 1L)
-          }
-        })
+      dq_values$sort_col <- i
+      dq_values$sort_dir <- dirs[input[[s]]]
       }
     }, ignoreInit = TRUE)
   })
 
   shiny::observeEvent(list(dq_values$sort_col, dq_values$sort_dir), {
-    dq_values[[context]] <- sort_data(dq_values[[context]], dq_values$sort_dir, dq_values$sort_col)
-    if (paged) {
+    s <- sorts[dq_values$sort_col]
+    if (length(s) == 1L && !is.na(s)) {
+      dq_values[[context]] <- sort_data(dq_values[[context]], dq_values$sort_dir, dq_values$sort_col)
+      lapply(sorts[sorts != s], function(n) {
+        if (length(input[[n]]) == 1L && input[[n]] != 1L) {
+          ignore[n] <<- TRUE
+          update_icon_state_button(session, n, value = 1L)
+        }
+      })
+    }
+    if (length(page_size) > 0L && page_size > 0L) {
       size <- as.integer(input[[paste0("sel_", context, "_pageSize")]])
+      if (length(size) == 0L) size <- page_size
       num <- input[[paste0("num_", context, "_page")]]
       dq_values[[page_id]] <- update_page(dq_values[[context]], context, num, size, session)
     }
-  }, ignoreInit = TRUE)
+  })
   sorts
 }
 
