aed4f8accd9c6edeed8cde6c942ea366b2a3b044
richard.kunze <richard.kunze@daqana.com>
2019-01-28 13:55:01 +0100

    Add popover function

---
+++ R/dq_popover.R
@@ -0,0 +1,23 @@
+dq_pop <- function(x, title, content, trigger = NULL, direction = NULL, options = NULL) {
+  arg_list <- not_null(list(
+    tag = x, title = title, "data-trigger" = trigger, "data-toggle" = "popover",
+    "data-placement" = direction, "data-content" = content
+  ))
+  x <- do.call(shiny::tagAppendAttributes, arg_list)
+  id <- x$attribs$id
+  if (is.null(id)) id <- paste0(sample(letters, 10), collapse = "")
+  shiny::tagList(
+    x, tags$script(paste0("$('#", id, "').popover(", jsonlite::toJSON(
+      options, auto_unbox = TRUE
+    ), ")"), if (isTRUE(trigger == "manual")) {
+      paste0(
+        "$('#", id, "').popover('show');",
+        "$('#' + $('#", id, "').attr('aria-describedby')).addClass('hidden');",
+        "$('#", id, "').click(function(e) {",
+        "$('#' + $(this).attr('aria-describedby')).toggleClass('hidden');",
+        "});"
+        # use focus/focusout instead
+      )
+    })
+  )
+}
